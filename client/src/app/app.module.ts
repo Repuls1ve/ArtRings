fd638c5e7c0e0d9fd414f8c51d8fff939e96c3d4
Repuls1ve <repulsive@list.ru>
2022-01-15 17:57:07 +0300

    header top nav

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './routing/app-routing.module'
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 import { FooterComponent } from './components/footer/footer.component'
 import { FlexLayoutModule } from '@angular/flex-layout'
+import { HeaderComponent } from './components/header/header.component'
+import { TopBarComponent } from './components/top-bar/top-bar.component'
+import { SocialMediaComponent } from './components/social-media/social-media.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent,
+    TopBarComponent,
+    SocialMediaComponent
   ],
   imports: [
     BrowserModule,
@@ -19,4 +25,4 @@ import { FlexLayoutModule } from '@angular/flex-layout'
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
