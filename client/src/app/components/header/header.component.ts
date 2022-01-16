cd542b82274068e97fd3321c6b4b360e8cb5a863
Repuls1ve <repulsive@list.ru>
2022-01-16 11:56:23 +0300

    header

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core'
+import { MediaObserver } from '@angular/flex-layout'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(public readonly media: MediaObserver) {}
+}
