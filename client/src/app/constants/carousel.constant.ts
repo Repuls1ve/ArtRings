c12941afe3066890ee76225c5e7e7793d4ad42ea
Repuls1ve <repulsive@list.ru>
2022-01-17 20:17:53 +0300

    carousel constant

---
+++ client/src/app/constants/carousel.constant.ts
@@ -0,0 +1,16 @@
+import { ICarouselImage } from '../components/carousel/carousel.component'
+
+export const HomeCarouselImages: ICarouselImage[] = [
+  {
+    source: '',
+    alt: 'rings'
+  },
+  {
+    source: '',
+    alt: 'rings'
+  },
+  {
+    source: '',
+    alt: 'rings'
+  }
+]
