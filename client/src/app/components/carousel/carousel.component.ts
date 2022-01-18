a92662d3ef43b252dba97e320cd67241a5e3dac2
Repuls1ve <repulsive@list.ru>
2022-01-18 17:45:54 +0300

    carousel

---
+++ client/src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core'
+
+export interface ICarouselImage {
+  source: string
+  alt: string
+  breakpoints?: string[]
+  sizes?: string[]
+}
+
+@Component({
+  selector: 'app-carousel',
+  templateUrl: './carousel.component.html',
+  styleUrls: ['./carousel.component.scss']
+})
+export class CarouselComponent {
+  @Input() public images: ICarouselImage[] = []
+
+  public active = 0
+
+  public isActive(image: ICarouselImage): boolean {
+    return this.active === this.images.indexOf(image)
+  }
+
+  public setActive(index: number): void {
+    this.active = index    
+  }
+
+  public toSrcset(breakpoints: ICarouselImage['breakpoints']): string | undefined {
+    console.log(breakpoints?.toString())
+    return breakpoints?.toString()
+  }
+}
--- client/src/app/constants/carousel.constant.ts
+++ client/src/app/constants/carousel.constant.ts
@@ -2,15 +2,27 @@ import { ICarouselImage } from '../components/carousel/carousel.component'
 
 export const HomeCarouselImages: ICarouselImage[] = [
   {
-    source: '',
-    alt: 'rings'
+    source: '/assets/img/rings-3840w.png',
+    alt: 'rings',
+    breakpoints: [
+      '/assets/img/rings-750w.png 750w',
+      '/assets/img/rings-3840w.png 3840w'
+    ]
   },
   {
-    source: '',
-    alt: 'rings'
+    source: '/assets/img/rings-3840w.png',
+    alt: 'rings',
+    breakpoints: [
+      '/assets/img/rings-750w.png 750w',
+      '/assets/img/rings-3840w.png 3840w'
+    ]
   },
   {
-    source: '',
-    alt: 'rings'
+    source: '/assets/img/rings-3840w.png',
+    alt: 'rings',
+    breakpoints: [
+      '/assets/img/rings-750w.png 750w',
+      '/assets/img/rings-3840w.png 3840w'
+    ]
   }
 ]
