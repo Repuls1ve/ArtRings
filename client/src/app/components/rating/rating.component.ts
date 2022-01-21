dc64bd2be2bd6bd560810faa31cc0d0f32ea4989
Repuls1ve <repulsive@list.ru>
2022-01-21 13:25:26 +0300

    rating scale

---
+++ client/src/app/components/rating/rating.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+type star = 'semi' | 'filled' | 'outlined'
+
+@Component({
+  selector: 'app-rating',
+  templateUrl: './rating.component.html',
+  styleUrls: ['./rating.component.scss']
+})
+export class RatingComponent implements OnInit {
+  @Input()
+  public rating!: number
+
+  public stars: star[] = []
+
+  ngOnInit(): void {
+    this.fillStars()
+    console.log(this.stars)
+  }
+
+  public starSource(star: star): string {
+    const baseSource = '/assets/svg/'
+    switch(star) {
+      case 'filled':
+        return baseSource + 'star-filled.svg'
+      case 'outlined':
+        return baseSource + 'star-outlined.svg'
+      case 'semi': 
+        return baseSource + 'star-semi.svg'
+    }
+  }
+
+  private fillStars(): void {
+    this.stars = [1, 2, 3, 4, 5].map(num => {
+      if (num <= this.rating) {
+        return 'filled'
+      }
+      else if (num - this.rating < 1) {
+        return 'semi'
+      }
+      else {
+        return 'outlined'
+      }
+    })
+  }
+}
