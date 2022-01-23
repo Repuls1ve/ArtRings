2068b87769662910efd60e7f76ae3de412186fa6
Repuls1ve <repulsive@list.ru>
2022-01-23 14:22:57 +0300

    product model

---
+++ client/src/app/models/product.model.ts
@@ -0,0 +1,38 @@
+import { ISliderImage } from '../components/slider/slider.component'
+
+export interface IProduct {
+  identifier: string
+  description: string
+  prices: IProductPrices
+  sizes: IProductSizes
+  reviews: IProductReview[]
+  images: IProductImage[]
+  discount: IProductDiscount | null
+  production: number
+  material: string
+  rating: number
+  stock: boolean
+}
+
+export interface IProductPrices {
+  [currency: string]: number
+}
+
+export interface IProductSizes {
+  male: number[]  
+  female: number[]
+}
+
+export interface IProductDiscount {
+  previous: IProductPrices
+}
+
+export interface IProductReview {
+  author: string
+  content: string
+  rating: number
+}
+
+export interface IProductImage extends ISliderImage {}
+
+
