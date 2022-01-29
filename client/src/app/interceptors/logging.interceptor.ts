f0c593b4a65890b0ebe67888485bf963c6315013
Repuls1ve <repulsive@list.ru>
2022-01-29 19:45:23 +0300

    logging interceptor

---
+++ client/src/app/interceptors/logging.interceptor.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core'
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpResponse,
+  HTTP_INTERCEPTORS
+} from '@angular/common/http'
+import { finalize, Observable, tap } from 'rxjs'
+
+type status = 'succeded' | 'error'
+
+@Injectable()
+export class LoggingInterceptor implements HttpInterceptor {
+  private elapsed!: number
+  private status!: status
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const nowTime = Date.now()
+
+    return next.handle(request).pipe(
+      tap(event => {
+        if (event instanceof HttpResponse) {
+          this.status = 'succeded'
+        }
+        else {
+          this.status = 'error'
+        }
+      }),
+      finalize(() => {  
+        this.elapsed = Date.now() - nowTime
+        console.warn(`${request.method} ${request.urlWithParams} ${this.status} in ${this.elapsed}ms`)
+      })
+    )
+  }
+}
+
+export const LoggingInterceptorProvider = [
+  { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true}
+]
