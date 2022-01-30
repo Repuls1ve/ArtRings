695d20a622a88a8807f3b5b5ff19e7e9fa8e14a0
Repuls1ve <repulsive@list.ru>
2022-01-30 20:47:28 +0300

    product schema

---
+++ server/src/products/schemas/product.schema.ts
@@ -0,0 +1,75 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
+import { Document } from 'mongoose'
+import { IProduct } from '../interfaces/product.interface'
+
+export type ProductDocument = Product & Document
+
+@Schema()
+export class Product implements IProduct {
+  @Prop({
+    type: String,
+    required: true
+  })
+  identifier: IProduct['identifier']
+
+  @Prop({
+    type: String,
+    required: true
+  })
+  description: IProduct['description']
+
+  @Prop({
+    type: Object,
+    required: true
+  })
+  prices: IProduct['prices']
+
+  @Prop({
+    type: [Object],
+    required: true
+  })
+  reviews: IProduct['reviews']
+
+  @Prop({
+    type: Object,
+    required: true
+  })
+  sizes: IProduct['sizes']
+
+  @Prop({
+    type: [Object],
+    required: true
+  })
+  images: IProduct['images']
+
+  @Prop({
+    type: Object
+  })
+  discount: IProduct['discount']
+
+  @Prop({
+    type: Date,
+    required: true
+  })
+  production: IProduct['production']
+
+  @Prop({
+    type: String,
+    required: true
+  })
+  material: IProduct['material']
+
+  @Prop({
+    type: Number,
+    required: true
+  })
+  rating: IProduct['rating']
+
+  @Prop({
+    type: Boolean,
+    required: true
+  })
+  stock: IProduct['stock']
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product)
