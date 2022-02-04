17f8c9128d4a507cd6b37b3e52d57fbfee261a87
Repuls1ve <repulsive@list.ru>
2022-02-04 11:48:37 +0300

    gift decoration component

---
+++ client/src/app/components/gift-decoration/gift-decoration.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-gift-decoration',
+  templateUrl: './gift-decoration.component.html',
+  styleUrls: ['./gift-decoration.component.scss']
+})
+export class GiftDecorationComponent {}
