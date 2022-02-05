7a30825797cc6c2183f5a1bc2ca8f5b773d7d742
Repuls1ve <repulsive@list.ru>
2022-02-05 14:59:22 +0300

    catalog guard

---
+++ client/src/app/guards/catalog.guard.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core'
+import { ActivatedRouteSnapshot, CanActivate, Router, UrlTree } from '@angular/router'
+import { AllowedCategories } from '../constants/catalog.constant'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CatalogGuard implements CanActivate {
+  constructor(private readonly router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot): boolean | UrlTree {
+    const shouldRedirect = !AllowedCategories.includes(route.params['category']) || !Number(route.queryParams['page'])
+    if (shouldRedirect) {
+      return this.router.parseUrl('/catalog/wedding?page=1')
+    } else {
+      return true
+    }
+  }
+}
