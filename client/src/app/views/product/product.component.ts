4c8fba5c9b9cdfeb06681224e8446a4107ee9767
Repuls1ve <repulsive@list.ru>
2022-02-10 19:05:29 +0300

    viewed section

--- client/src/app/views/product/product.component.ts
+++ client/src/app/views/product/product.component.ts
@@ -2,9 +2,14 @@ import { Component, OnInit } from '@angular/core'
 import { MediaObserver } from '@angular/flex-layout'
 import { ActivatedRoute } from '@angular/router'
 import { Store } from '@ngrx/store'
+import { IProduct } from 'src/app/models/product.model'
 import { AppState } from 'src/app/store/app.state'
 import { loadProduct } from 'src/app/store/product/product.actions'
 import { selectProduct } from 'src/app/store/product/product.selectors'
+import { addViewed } from 'src/app/store/viewed/viewed.actions'
+import { selectViewed } from 'src/app/store/viewed/viewed.selectors'
+import { addWished, removeWished } from 'src/app/store/wishlist/wishlist.actions'
+import { selectWishlist } from 'src/app/store/wishlist/wishlist.selectors'
 
 type branch = 'description' | 'reviews'
 
@@ -14,7 +19,9 @@ type branch = 'description' | 'reviews'
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+  public readonly wishlist$ = this.store.select(selectWishlist)
   public readonly product$ = this.store.select(selectProduct)
+  public readonly viewed$ = this.store.select(selectViewed)
   public branch: branch = 'description'
 
   constructor(
@@ -24,12 +31,23 @@ export class ProductComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => 
+    this.route.params.subscribe(params => {
       this.store.dispatch(loadProduct({id: params['id']}))
-    )
+      this.store.dispatch(addViewed({data: params['id']}))
+    })
   }
 
   public setBranch(branch: branch): void {
     this.branch = branch
+  }
+
+  public onWishedClick(wishlist: AppState['wishlist'], id: IProduct['_id']): void {
+    this.isWished(wishlist, id) ?
+    this.store.dispatch(removeWished({data: id})) :
+    this.store.dispatch(addWished({data: id}))
+  }
+
+  public isWished(wishlist: AppState['wishlist'], id: IProduct['_id']): boolean {
+    return wishlist.data.items?.some(product => product._id === id)
   }
 }
