22745e7d610a64aa38c26a0618737e9fe7fbcbbc
Repuls1ve <repulsive@list.ru>
2022-02-10 13:07:44 +0300

    wishlist/cart count

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core'
 import { MediaObserver } from '@angular/flex-layout'
+import { Store } from '@ngrx/store'
+import { AppState } from 'src/app/store/app.state'
+import { selectCartQuantity } from 'src/app/store/cart/cart.selectors'
+import { selectWishlistQuantity } from 'src/app/store/wishlist/wishlist.selectors'
 
 @Component({
   selector: 'app-header',
@@ -7,9 +11,14 @@ import { MediaObserver } from '@angular/flex-layout'
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  public wishlistQty$ = this.store.select(selectWishlistQuantity)
+  public cartQty$ = this.store.select(selectCartQuantity)
   public navbarClosed = true
 
-  constructor(public readonly media: MediaObserver) {}
+  constructor(
+    public readonly media: MediaObserver,
+    private readonly store: Store<AppState>  
+  ) {}
 
   toggleNavbar(): void {
     this.navbarClosed = !this.navbarClosed
