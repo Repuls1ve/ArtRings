a91289b7bbd854f49b724b4d0a295dbcd6823711
Repuls1ve <repulsive@list.ru>
2022-02-18 20:09:10 +0300

    layout fixes

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
 import { MediaObserver } from '@angular/flex-layout'
+import { NavigationEnd, Router } from '@angular/router'
 import { Store } from '@ngrx/store'
+import { filter, Subscription } from 'rxjs'
 import { AppState } from 'src/app/store/app.state'
 import { selectGuest } from 'src/app/store/guest/guest.selectors'
 
@@ -9,16 +11,36 @@ import { selectGuest } from 'src/app/store/guest/guest.selectors'
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
+  private readonly subscriptions: Subscription[] = []
+
   public readonly guest$ = this.store.select(selectGuest)
   public navbarClosed = true
 
   constructor(
     public readonly media: MediaObserver,
-    private readonly store: Store<AppState>  
+    private readonly store: Store<AppState>,
+    private readonly router: Router,
   ) {}
 
-  toggleNavbar(): void {
-    this.navbarClosed = !this.navbarClosed
+  public ngOnInit(): void {
+    const subscription = this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => this.closeNavbar())
+    this.subscriptions.push(subscription)
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.forEach(subscription => 
+      subscription.unsubscribe()
+    )
+  }
+
+  public closeNavbar(): void {
+    this.navbarClosed = true
+  }
+
+  public openNavbar(): void {
+    this.navbarClosed = false
   }
 }
